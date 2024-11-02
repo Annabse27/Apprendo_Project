9423b1d4d880e728e696d2ca1140a9480809a8d4
Annabse27 <annabse27@gmail.com>
2024-11-02 21:27:54 +0100

    MANAGE.PY: инициализация Django перед выполнением команд, улучшена обработка ошибок.

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -8,6 +6,8 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
+        import django
+        django.setup()  # Инициализируем Django после установки DJANGO_SETTINGS_MODULE
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(

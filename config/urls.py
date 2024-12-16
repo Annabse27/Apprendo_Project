c4a3d15fdd7d08c1b6393264acbae5c15c45c815
Annabse27 <annabse27@gmail.com>
2024-12-17 00:44:07 +0100

    КОНФИГУРАЦИОННЫЙ ПАКЕТ:

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import ProtectedView
+from django.http import HttpResponse
 
 
 # Импорты для документации
@@ -17,6 +18,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', lambda request: HttpResponse('Привет, мир!')),  # временная заглушка для проверки
 
 ]
 
